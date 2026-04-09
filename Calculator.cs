d868a7dbb2d0effcffdfb9ff6578f4beb083821a
dhani <r.romdhani@students.amikom.ac.id>
2026-04-09 15:51:44 +0700

    Inisialisasi project

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,16 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil Penjumlahan: {0} + {1} = {2}", a, b, Penjumlahan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar")
+		Console.ReadKey();
+	}
+	static int Penjumlahan(int a, int b)
+	{
+		return a + b;
 	}
 }
